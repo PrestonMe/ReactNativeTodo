2c219832114d2198396e6e418b57cf4176520980
preston <mechampreston@gmail.com>
2017-06-18 02:27:24 -0600

    Resetting android to iOS code

--- index.android.js
+++ index.android.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
 
 class Todo extends Component {
   constructor() {
     super();
     this.state = {
-      todos: [1,2,3]
+      todos: [1,2,3],
+      newTodo: ''
     }
   }
+
+  handleChange(e) {
+    const { value } = e.target;
+    this.setState({newTodo: value})
+  }
+
+  handlePress() {
+
+  }
+
   render() {
     return(
       <View style={styles.container}>
+        <TextInput value={this.state.newTodo} onChange={this.handleChange.bind(this)} />
+        {/* we can use touchablehighlight like a button/ we use onpress since there is no clicks
+          on mobile devices*/}
+        <TouchableHighlight onPress={this.handlePress.bind(this)}>
+          <Text>Tap Me</Text>
+        </TouchableHighlight>
         {this.state.todos.map(todo => <Text>{todo}</Text>)}
       </View>
       // <View style={styles.container}>
@@ -32,9 +49,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignTiems:
-  }
-})
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 // Registering our root component is like doing the render method inside of React,
 // This is the react-native version of that
